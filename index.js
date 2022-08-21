dedd75e20ad484f71acd51986b6deac8909eb05f
SanderLoman <sanderfeitsma13@gmail.com>
2022-08-21 03:01:16 +0200

    commitlol

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your whale tracker script here!
+const ethers = require("ethers")
+const rpcURL =
+    "https://eth-mainnet.g.alchemy.com/v2/gClkJVB2E2C-wUuolw-mWwGwmxjov0Y0"
+const provider = new ethers.providers.JsonRpcProvider(rpcURL)
+
+const main = async () => {
+    const block = await provider.getBlockNumber()
+    console.log(block)    
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
